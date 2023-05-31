19b18fc9f86aab8ddd6929460dc9a99b38414cb1
Josh <joshua02k@gmail.com>
2023-05-31 18:33:11 -0400

    helper function and modules

---
+++ modules/stream_to_message.js
@@ -0,0 +1,8 @@
+function stream_to_message(stream, callback, ...args){
+    //...args here is an array of arguments
+    let body = "";
+    stream.on("data", (chunk) => body += chunk);
+    stream.on("end", () => callback(body, ...args));
+}
+
+module.exports = stream_to_message;
