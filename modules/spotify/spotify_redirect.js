41e6aac753e7c7b126d84ed310c42426d766c3dd
Josh <joshua02k@gmail.com>
2023-06-01 15:07:09 -0400

    server cleanup

--- modules/spotify/spotify_redirect.js
+++ modules/spotify/spotify_redirect.js
@@ -1,16 +1,20 @@
 const querystring = require("querystring");
+const generate_session = require('../generate_session');
 
-function spotify_redirect(res, state){
-    console.log("Redirecting!");
-    const auth_endpoint = "https://accounts.spotify.com/authorize";
-    const query_params = querystring.stringify({
-        client_id: process.env.SPOTIFY_ID,
-        response_type: "code",
-        redirect_uri: "http://localhost:3000/spotify_code",
-        scope: "user-read-currently-playing user-read-private user-read-email",
-        state: state
-    });
-    res.redirect(302, `${auth_endpoint}?${query_params}`);
+function spotify_redirect(){
+    return (req, res, next) => {
+        const state = generate_session();
+        console.log("Redirecting to Spotify!");
+        const auth_endpoint = "https://accounts.spotify.com/authorize";
+        const query_params = querystring.stringify({
+            client_id: process.env.SPOTIFY_ID,
+            response_type: "code",
+            redirect_uri: "http://localhost:3000/spotify_code",
+            scope: "user-read-currently-playing user-read-private user-read-email",
+            state: state
+        });
+        res.redirect(302, `${auth_endpoint}?${query_params}`);
+    }
 }
 
 module.exports = spotify_redirect;
--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 const express = require("express");
 require("dotenv").config();
-const crypto = require("crypto");
 const app = express();
 const port = 3000;
 
 //modules
 const spotify_redirect = require("./modules/spotify/spotify_redirect.js");
 const spotify_token = require("./modules/spotify/spotify_token.js");
+const github_redirect = require("./modules/github/github_redirect.js");
+
+//db
 const user_sessions = require("./modules/database/user_sessions.js");
 
 // Home page route within this
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended: false}));
 
-app.post("/login", (req,res) => {
-    const state = crypto.randomBytes(20).toString("hex");
-    user_sessions.set_user_value(state);
-    spotify_redirect(res, state);
+app.post("/login", spotify_redirect() ,(req,res) => {
+    console.log("Redirect should've been handled");
 });
 
-app.get("/spotify_code", spotify_token(),(req, res) => {
+app.get("/spotify_code", spotify_token(), github_redirect(),(req, res) => {
     if(req.query.error){
         return res.redirect("/");
-    }else{
-        res.send(user_sessions.get_user(req.query.state));
     }
 });
 
-// app.get("/github_code", github_token(), (req, res) => {
-//     res.send("Received Github Token :)");
-// });
+app.get("/github_code", (req, res) => {
+    res.send("Received Github Token :) it's " + req.query.code);
+});
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
