da86c8b90a186625fee3cda9a7f879d18dc2e027
Josh <joshua02k@gmail.com>
2023-06-01 15:08:46 -0400

    cleanup / added middleware next

--- modules/spotify/get_spotify_id.js
+++ modules/spotify/get_spotify_id.js
@@ -6,7 +6,7 @@ const user_sessions = require("../database/user_sessions");
 const get_now_playing = require("./get_now_playing.js");
 const refresh_token = require("./refresh_token.js");
 
-function get_spotify_id(state, token){
+function get_spotify_id(state, token, next){
     const endpoint = "https://api.spotify.com/v1/me";
     const options = {
         method: "GET",
@@ -20,12 +20,13 @@ function get_spotify_id(state, token){
             const spotify_id = response_obj.id;
             if(user_sessions.user_exists(spotify_id)){
                 console.log("user is already logged in, not making another session");
+                //will hang unless dealt w
                 return;
             }else{
                 const user = user_sessions.get_user(state);
                 console.log(user, "USER");
                 user_sessions.set_user_value(state, "spotify_id", response_obj.id);
-                console.log(user_sessions.get_user(state));
+
 
                 //keep this here for now, but fr need to put this in after github auth
                 //set access token for inital request
@@ -34,6 +35,8 @@ function get_spotify_id(state, token){
                 refresh_token.refresh_token(user.spotify_refresh);
                 //calls get now playing function to start fetching and logging current track
                 get_now_playing();
+
+                next();
             }
         });
     }).end();
