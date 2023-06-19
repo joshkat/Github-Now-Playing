6c9a77d1b6a19db43cd548adbc26f7ad7d3f7d2a
Josh <joshua02k@gmail.com>
2023-06-19 17:12:35 -0400

    added env port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //modules
 const spotify_redirect = require("./modules/spotify/spotify_redirect.js");
